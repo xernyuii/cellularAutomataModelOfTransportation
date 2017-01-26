ea7ae8389e7f5b50795c28915a30e4f4d5c5abdd
xernyuii <hfutxc.xern@gmail.com>
2017-01-27 00:04:02 +0800

    add

---
+++ clear_boundary.m
@@ -0,0 +1,10 @@
+function plaza = clear_boundary(plaza)
+global GETOUTNUM;
+global GETOUTITEM;
+[a,b] = size(plaza);
+for i = 1:b
+    if plaza(a,i) > 0
+        plaza(a,i) = 0;
+        GETOUTNUM(GETOUTITEM)=GETOUTNUM(GETOUTITEM)+1;
+    end
+end
